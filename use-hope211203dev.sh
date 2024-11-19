29d369607ef7994deacda811fb72cff81f32b578
esmart211203 <esmart211203@gmail.com>
2024-11-19 16:55:59 +0700

    first commit

---
+++ use-hope211203dev.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+git config user.name "hope211203dev"
+git config user.email "hope211203dev@gmail.com"
+git config core.sshCommand "ssh -i /c/Users/esmar/.ssh/id_rsa_hope211203dev"
+echo "Switched to account hope211203dev"
